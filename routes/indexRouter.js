963c7ff4958fdb43808e50ca7ba6d5117c5b300b
Victor Mwadime <victormwadime@gmail.com>
2024-11-12 11:18:54 +0300

    fix typo

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -16,7 +16,7 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.render(index, { messages: messages });
+  res.render("index", { messages: messages });
 });
 
 module.exports = indexRouter;
