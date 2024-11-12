ad128b994f1c10ef0f74cb0475e18f560e8396f6
Victor Mwadime <victormwadime@gmail.com>
2024-11-12 11:07:12 +0300

    create index router

---
+++ routes/indexRouter.js
@@ -0,0 +1,22 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello world",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+const { Router } = require("express");
+
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => {
+  res.render(index, { messages: messages });
+});
+
+module.exports = indexRouter;
